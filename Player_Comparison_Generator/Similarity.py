eba7395a0874698e27a4427fa95a2c409988215b
Robert Anderson <rba739@gmail.com>
2017-07-07 21:07:20 -0400

    got Similarity() to run for all comparisons

--- Player_Comparison_Generator/Similarity.py
+++ Player_Comparison_Generator/Similarity.py
@@ -6,7 +6,7 @@ dataSetI = [3,45,7,2]
 dataSetII = [2,54,13,15]
 dataSetIII = [7,2,1,3]
 
-test_data = [30,900,8.1,15.5,.524,6.1,10.9,.563,2.0,4.6,.433,6.8,9.1,.755,11.8,3.2,2.8,2.7,25.1]
+test_data = [30,900,8.1,15.5,.524,6.1,10.9,.563,2.0,4.6,.433,6.8,9.1,.755,11.8,3.2,2.8,2.7,25.1] #danny granger last season
 test_data2 = [32,868,3.5,8.1,.426,2.2,4.5,.485,1.3,3.6,.354,1.1,2.1,.533,4.4,6.7,2.1,0.2,9.3]
 
 
@@ -17,12 +17,14 @@ result3 = 1 - spatial.distance.cosine(dataSetII, dataSetIII)
 
 def Similarity(prospect, comparison):
     sim_score = 1 - spatial.distance.cosine(prospect, comparison)
-    print sim_score
+    print (sim_score )
 
 
-#print result,result2,result3
+for data in sql.read_from_db():
+    Similarity(test_data, data[6:])
+    print '------------------------------'
 
-Similarity(test_data, sql.read_from_db())
-Similarity(test_data2, sql.read_from_db())
 sql.c.close()
 sql.conn.close()
+
+#trying to figure out how to print both tag and sim score
